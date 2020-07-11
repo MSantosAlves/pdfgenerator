c00654646f1a70aab3698230e94c416d0a6a7d72
guilherme natal <guilhermenatal47@gmail.com>
2020-07-10 22:23:03 -0300

    v1

--- src/components/Document/index.js
+++ src/components/Document/index.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react'
 import {
   Document,
   PDFDownloadLink,
   Image,
   PDFViewer,
-} from "@react-pdf/renderer";
+} from '@react-pdf/renderer'
 
-import { Button } from "../../pages/Home/styles";
+import { Button } from '../../pages/Home/styles'
 import {
   Page,
   Category,
@@ -14,13 +14,15 @@ import {
   Grid,
   Product,
   Name,
+  StepGrid,
   Price,
   LogoBox,
   Email,
   Footer,
   StyledText,
   Divider,
-} from "./styles";
+  StepBox,
+} from './styles'
 
 // import TestImg from "../../assets/test.jpg";
 
@@ -44,27 +46,88 @@ import {
 //   ],
 // };
 
+const mourningSteps = [
+  {
+    title: 'Passo 1',
+    content: 'Limpar com sabonete ou gel de limpeza. A cada 15 dias Esfoliação',
+  },
+  {
+    title: 'Passo 2',
+    content: 'Aplicar 2	Água micelar ',
+  },
+  {
+    title: 'Passo 3',
+    content: 'Aplicar 3 Vitamina c',
+  },
+  {
+    title: 'Passo 4',
+    content: 'Aplicar 4	Hidratante',
+  },
+  {
+    title: 'Passo 5',
+    content: 'Aplicar	o Protetor Solar',
+  },
+  {
+    title: 'Passo 6',
+    content: 'Aplicar o	Protetor/Reparador labial',
+  },
+]
+
+const nightSteps = [
+  {
+    title: 'Passo 1',
+    content:
+      'Limpar com Sabonete ou Gel de Limpeza A cada 15 dias Máscara Facia',
+  },
+  {
+    title: 'Passo 2',
+    content: 'Aplicar o Tônico',
+  },
+  {
+    title: 'Passo 3',
+    content:
+      'Aplicar os	Tratamentos (Antissinais/Antiacne com cremes ou séruns específicos)',
+  },
+  {
+    title: 'Passo 4',
+    content: 'Usar os produtos de	Cuidado com a Área dos olhos',
+  },
+  {
+    title: 'Passo 5',
+    content: 'Aplicar	o	Hidratante',
+  },
+]
+
 // Create Document Component
 const MyDocument = ({ data, user }) => (
   <Document>
-    {console.log("doc products", data)}
+    {console.log('doc products', data)}
 
-    <Page size="A4" style={{ border: "1px solid red" }}>
+    <Page size="A4" style={{ border: '1px solid red' }}>
       <LogoBox>
         <Image
-          src={"/LogoGeral.jpg"}
+          src={'/LogoHeader.jpg'}
           style={{
-            width: "100px",
-            height: "100px",
-            objectFit: "contain",
-            marginBottom: "40px",
+            width: '300px',
+            height: '80px',
+            objectFit: 'contain',
+            marginBottom: '10px',
           }}
         />
       </LogoBox>
-      {Object.keys(data).map((category) => {
+      <Footer>
+        <StyledText>
+          Veja a seguir suas recomendações de dermocosméticos. Siga até o fim
+          para saber como ganhar até R$20,00
+        </StyledText>
+      </Footer>
+      {Object.keys(data).map((category, index) => {
         return (
           <>
-            <CategoryBox key={category}>
+            <CategoryBox
+              key={category}
+              break={index > 0 && (index + 1) % 3 === 0}
+            >
               <Category>{category}</Category>
               <Grid>
                 {data[category].map((product) => (
@@ -72,9 +135,9 @@ const MyDocument = ({ data, user }) => (
                     <Image
                       src={product.image.base64}
                       style={{
-                        width: "200x",
-                        height: "200px",
-                        objectFit: "cover",
+                        width: '150px',
+                        height: '100px',
+                        objectFit: 'contain',
                       }}
                     />
                     <Name>{product.name}</Name>
@@ -85,28 +148,61 @@ const MyDocument = ({ data, user }) => (
             </CategoryBox>
             <Divider />
           </>
-        );
+        )
       })}
       {user && <Email>{user.email}</Email>}
+      <Footer break>
+        <StyledText>Rotina matinal</StyledText>
+      </Footer>
+      <StepGrid>
+        {mourningSteps.map((step) => {
+          return (
+            <StepBox>
+              <StyledText>{step.title}</StyledText>
+              <StyledText>{step.content}</StyledText>
+            </StepBox>
+          )
+        })}
+      </StepGrid>
       <Footer>
+        <StyledText>Rotina noturna</StyledText>
+      </Footer>
+      <StepGrid>
+        {nightSteps.map((step) => {
+          return (
+            <StepBox>
+              <StyledText>{step.title}</StyledText>
+              <StyledText>{step.content}</StyledText>
+            </StepBox>
+          )
+        })}
+      </StepGrid>
+      <Footer break>
         <StyledText>
-          Gostou das dicas? Compre os produtos recomendados no site de sua
-          preferência e receba R$10,00 na sua conta para cada R$100,00 em
-          compras de dermocosméticos.
+          Ganhe R$10,00 nas compras acima de R$150,00 ou R$20,00 nas compras
+          acima de R$200,00.{' '}
+        </StyledText>
+        <StyledText>Regras:</StyledText>
+        <StyledText>
+          1) Basta fazer uma compra de produtos recomendados pela Encontre
+          Beleza no seu site de preferência.
         </StyledText>
         <StyledText>
-          Basta enviar uma imagem do comprovante de compra para
-          marketing@encontrebeleza.com e os dados da sua conta para depósito.
+          2) Enviar um email para marketing@encontrebeleza.com com uma impressão
+          do seu pedido de compra, com seus dados pessoais e dados da compra bem
+          legíveis.
         </StyledText>
-        <StyledText>Limitado ao pagamento de R$20,00 por CPF.</StyledText>
         <StyledText>
-          Importante: O depósito será realizado no mesmo CPF que consta na nota
-          fiscal de compra do dermocosmético.
+          3) Incluir no mesmo email o seu Nome Completo, Banco, Agência, Conta e
+          CPF para que possamos efetuar o depósito.
+        </StyledText>
+        <StyledText>
+          Importante: promoção limitada a R$20,00 por CPF.”
         </StyledText>
       </Footer>
     </Page>
   </Document>
-);
+)
 
 export const DownloadLink = ({ data }) => (
   <div>
@@ -119,10 +215,10 @@ export const DownloadLink = ({ data }) => (
       </Button>
     </PDFDownloadLink>
   </div>
-);
+)
 
 export const Preview = ({ data, user }) => (
-  <PDFViewer style={{ width: "100%", minHeight: "900px" }}>
+  <PDFViewer style={{ width: '100%', minHeight: '900px' }}>
     <MyDocument data={data} user={user} />
   </PDFViewer>
-);
+)
--- src/components/Document/styles.js
+++ src/components/Document/styles.js
@@ -1,67 +1,82 @@
-import styled from "@react-pdf/styled-components";
+import styled from '@react-pdf/styled-components'
 
 export const Page = styled.Page`
   flex-direction: column;
   background: #fff;
   align-items: center;
-`;
+`
 
 export const CategoryBox = styled.View`
   padding: 0px 12% 10px;
-`;
+`
 
 export const Grid = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-between;
-`;
+`
 
 export const Divider = styled.View`
   background: #ccc;
   height: 2px;
   margin: 20px 12%;
-`;
+`
 
 export const LogoBox = styled.View`
   margin: 10px 0px;
   width: 100%;
   align-items: center;
-`;
+`
 
 export const Product = styled.View`
   margin: 10px 0px;
   width: 200px;
   align-items: center;
-`;
+`
 
 export const Category = styled.Text`
   margin: 0px 0px 5px;
   color: #320054;
   font-weight: bold;
   font-size: 32px;
-`;
+`
 export const Name = styled.Text`
   margin: 5px 0px;
   font-size: 18px;
-`;
+`
 export const Price = styled.Text`
   margin: 5px 0px;
   font-weight: bold;
   font-size: 18px;
-`;
+`
 
 export const Email = styled.Text`
   text-align: center;
   margin: 5px auto;
   color: #320054;
   font-size: 18px;
-`;
+`
 
 export const Footer = styled.View`
   padding: 5px 20px;
-`;
+`
 
 export const StyledText = styled.Text`
   margin-bottom: 5px;
   font-size: 14px;
-`;
+`
+
+export const StepGrid = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+export const StepBox = styled.View`
+  margin: 10px 0px;
+  width: 150px;
+  padding: 5px 20px;
+  border: solid 2px #a16bac;
+  height: 80px;
+  align-items: center;
+`
